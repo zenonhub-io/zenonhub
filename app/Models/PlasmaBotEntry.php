4f810828a5f6cf662787633bbc3c65b419674787
DigitalSloth <digitals1oth@proton.me>
2023-05-22 22:29:12 +0100

    Fix scope

--- app/Models/PlasmaBotEntry.php
+++ app/Models/PlasmaBotEntry.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Query\Builder;
 
 class PlasmaBotEntry extends Model
 {
