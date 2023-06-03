85421862550dc3c48fbc116349882f5fff7abfa0
DigitalSloth <digitals1oth@proton.me>
2023-06-03 10:58:37 +0100

    Add debug logging

--- app/Actions/ProcessBlock.php
+++ app/Actions/ProcessBlock.php
@@ -6,6 +6,7 @@ use App\Jobs\Alerts\WhaleAlert;
 use App\Jobs\ProcessAccountBalance;
 use App\Models\Nom\Account;
 use App\Models\Nom\AccountBlock;
+use Log;
 use Spatie\QueueableAction\QueueableAction;
 
 class ProcessBlock
@@ -22,6 +23,10 @@ class ProcessBlock
 
     public function execute(): void
     {
+        Log::debug('Processing block '.$this->block->hash);
+        Log::debug('Whale alerts '.($this->fireWhaleAlerts ? 'Yes' : 'No'));
+        Log::debug('Account balances '.($this->processAccounts ? 'Yes' : 'No'));
+
         if ($this->block->data && $this->block->contract_method) {
             $jobClassName = $this->block->contract_method?->job_class_name;
             if ($jobClassName && class_exists($jobClassName)) {
@@ -30,17 +35,25 @@ class ProcessBlock
         }
 
         if ($this->fireWhaleAlerts && $this->block->amount > 0 && in_array($this->block->token_id, [1, 2])) {
+            Log::debug('Fire whale alerts');
             WhaleAlert::dispatch($this->block);
         }
 
         if ($this->processAccounts) {
+
+            $delay = now()->addSeconds(30);
+
             if ($this->block->account->address !== Account::ADDRESS_EMPTY) {
-                ProcessAccountBalance::dispatch($this->block->account)->delay(now()->addSeconds(30));
+                Log::debug('Dispatch account balances job '.$this->block->account->address);
+                ProcessAccountBalance::dispatch($this->block->account)->delay($delay);
             }
 
             if ($this->block->to_account->address !== Account::ADDRESS_EMPTY) {
-                ProcessAccountBalance::dispatch($this->block->to_account)->delay(now()->addSeconds(30));
+                Log::debug('Dispatch account balances job '.$this->block->to_account->address);
+                ProcessAccountBalance::dispatch($this->block->to_account)->delay($delay);
             }
         }
+
+        Log::debug('End processing block');
     }
 }
--- app/Events/Nom/AccountBlockCreated.php
+++ app/Events/Nom/AccountBlockCreated.php
@@ -11,9 +11,6 @@ class AccountBlockCreated
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    /**
-     * Create a new event instance.
-     */
     public function __construct(
         public AccountBlock $block,
         public bool $sendWhaleAlerts,
