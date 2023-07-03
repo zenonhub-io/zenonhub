fbd1cce8797c6dabf561e340898c1c0271a4c149
DigitalSloth <digitals1oth@proton.me>
2023-07-03 20:38:44 +0100

    add no results message for accounts and tokens

--- resources/views/livewire/explorer/accounts.blade.php
+++ resources/views/livewire/explorer/accounts.blade.php
@@ -66,101 +66,112 @@
         </div>
     </div>
     <div wire:loading.remove>
-        <div class="table-responsive">
-            <table class="table table-nowrap align-middle table-striped table-hover">
-                <thead>
-                <tr>
-                    <th>
-                        Address
-                    </th>
-                    <th>
-                        <button type="button" class="btn btn-sort" wire:click="sortBy('sent_blocks_count')">
-                            <x-table-sort-button :sort="$sort" :order="$order" check="sent_blocks_count" title="Height"/>
-                        </button>
-                    </th>
-                    <th>
-                        <button type="button" class="btn btn-sort" wire:click="sortBy('znn_balance')">
-                            <x-table-sort-button :sort="$sort" :order="$order" check="znn_balance" title="ZNN"/>
-                        </button>
-                    </th>
-                    <th>
-                        <button type="button" class="btn btn-sort" wire:click="sortBy('total_znn_balance')">
-                            <x-table-sort-button :sort="$sort" :order="$order" check="total_znn_balance" title="Total ZNN" tooltip="Includes ZNN locked in staking, pillars and sentinels"/>
-                        </button>
-                    </th>
-                    <th>
-                        <button type="button" class="btn btn-sort" wire:click="sortBy('qsr_balance')">
-                            <x-table-sort-button :sort="$sort" :order="$order" check="qsr_balance" title="QSR"/>
-                        </button>
-                    </th>
-                    <th>
-                        <button type="button" class="btn btn-sort" wire:click="sortBy('total_qsr_balance')">
-                            <x-table-sort-button :sort="$sort" :order="$order" check="total_qsr_balance" title="Total QSR" tooltip="Includes fused QSR and sentinel lockup"/>
-                        </button>
-                    </th>
-                    <th>
-                        <button type="button" class="btn btn-sort" wire:click="sortBy('updated_at')">
-                            <x-table-sort-button :sort="$sort" :order="$order" check="updated_at" title="Last active"/>
-                        </button>
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($data as $account)
+        @if ($data && $data->count())
+            <div class="table-responsive">
+                <table class="table table-nowrap align-middle table-striped table-hover">
+                    <thead>
                     <tr>
-                        <td>
-                            <div class="d-flex">
-                                <div class="pe-1 flex-grow-1"><x-address :account="$account" :eitherSide="10" breakpoint="lg"/></div>
-                                <div class="ps-1">
-                                    @if ($account->active_pillar)
-                                        <span class="ms-2 d-inline" data-bs-toggle="tooltip" data-bs-title="Pillar">
-                                            {!! svg('pillar', 'opacity-70', 'height: 18px') !!}
-                                        </span>
-                                    @endif
-                                    @if ($account->active_sentinel)
-                                        <span class="ms-2 d-inline" data-bs-toggle="tooltip" data-bs-title="Sentinel">
-                                            {!! svg('sentinel', '', 'width: 16px') !!}
-                                        </span>
-                                    @endif
-                                    @if ($account->is_embedded_contract)
-                                        <span class="d-inline" data-bs-toggle="tooltip" data-bs-title="Embedded contract">
-                                            <i class="bi bi-file-text-fill opacity-70"></i>
-                                        </span>
-                                    @endif
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            {{ number_format($account->sent_blocks_count) }}
-                        </td>
-                        <td>
-                            <span data-bs-toggle="tooltip" data-bs-title="{{ $account->displayZnnBalance() }}">
-                                {{ $account->displayZnnBalance(2) }}
-                            </span>
-                        </td>
-                        <td>
-                            <span data-bs-toggle="tooltip" data-bs-title="{{ $account->displayTotalZnnBalance() }}">
-                                {{ $account->displayTotalZnnBalance(2) }}
-                            </span>
-                        </td>
-                        <td>
-                            <span data-bs-toggle="tooltip" data-bs-title="{{ $account->displayQsrBalance() }}">
-                                {{ $account->displayQsrBalance(2) }}
-                            </span>
-                        </td>
-                        <td>
-                            <span data-bs-toggle="tooltip" data-bs-title="{{ $account->displayTotalQsrBalance() }}">
-                                {{ $account->displayTotalQsrBalance(2) }}
-                            </span>
-                        </td>
-                        <td>{{ $account->updated_at?->format(config('zenon.short_date_format')) }}</td>
+                        <th>
+                            Address
+                        </th>
+                        <th>
+                            <button type="button" class="btn btn-sort" wire:click="sortBy('sent_blocks_count')">
+                                <x-table-sort-button :sort="$sort" :order="$order" check="sent_blocks_count" title="Height"/>
+                            </button>
+                        </th>
+                        <th>
+                            <button type="button" class="btn btn-sort" wire:click="sortBy('znn_balance')">
+                                <x-table-sort-button :sort="$sort" :order="$order" check="znn_balance" title="ZNN"/>
+                            </button>
+                        </th>
+                        <th>
+                            <button type="button" class="btn btn-sort" wire:click="sortBy('total_znn_balance')">
+                                <x-table-sort-button :sort="$sort" :order="$order" check="total_znn_balance" title="Total ZNN" tooltip="Includes ZNN locked in staking, pillars and sentinels"/>
+                            </button>
+                        </th>
+                        <th>
+                            <button type="button" class="btn btn-sort" wire:click="sortBy('qsr_balance')">
+                                <x-table-sort-button :sort="$sort" :order="$order" check="qsr_balance" title="QSR"/>
+                            </button>
+                        </th>
+                        <th>
+                            <button type="button" class="btn btn-sort" wire:click="sortBy('total_qsr_balance')">
+                                <x-table-sort-button :sort="$sort" :order="$order" check="total_qsr_balance" title="Total QSR" tooltip="Includes fused QSR and sentinel lockup"/>
+                            </button>
+                        </th>
+                        <th>
+                            <button type="button" class="btn btn-sort" wire:click="sortBy('updated_at')">
+                                <x-table-sort-button :sort="$sort" :order="$order" check="updated_at" title="Last active"/>
+                            </button>
+                        </th>
                     </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
-        <div class="m-4 mt-2">
-            {{ $data->onEachSide(0)->links() }}
-        </div>
+                    </thead>
+                    <tbody>
+                    @foreach($data as $account)
+                        <tr>
+                            <td>
+                                <div class="d-flex">
+                                    <div class="pe-1 flex-grow-1"><x-address :account="$account" :eitherSide="10" breakpoint="lg"/></div>
+                                    <div class="ps-1">
+                                        @if ($account->active_pillar)
+                                            <span class="ms-2 d-inline" data-bs-toggle="tooltip" data-bs-title="Pillar">
+                                                {!! svg('pillar', 'opacity-70', 'height: 18px') !!}
+                                            </span>
+                                        @endif
+                                        @if ($account->active_sentinel)
+                                            <span class="ms-2 d-inline" data-bs-toggle="tooltip" data-bs-title="Sentinel">
+                                                {!! svg('sentinel', '', 'width: 16px') !!}
+                                            </span>
+                                        @endif
+                                        @if ($account->is_embedded_contract)
+                                            <span class="d-inline" data-bs-toggle="tooltip" data-bs-title="Embedded contract">
+                                                <i class="bi bi-file-text-fill opacity-70"></i>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                {{ number_format($account->sent_blocks_count) }}
+                            </td>
+                            <td>
+                                <span data-bs-toggle="tooltip" data-bs-title="{{ $account->displayZnnBalance() }}">
+                                    {{ $account->displayZnnBalance(2) }}
+                                </span>
+                            </td>
+                            <td>
+                                <span data-bs-toggle="tooltip" data-bs-title="{{ $account->displayTotalZnnBalance() }}">
+                                    {{ $account->displayTotalZnnBalance(2) }}
+                                </span>
+                            </td>
+                            <td>
+                                <span data-bs-toggle="tooltip" data-bs-title="{{ $account->displayQsrBalance() }}">
+                                    {{ $account->displayQsrBalance(2) }}
+                                </span>
+                            </td>
+                            <td>
+                                <span data-bs-toggle="tooltip" data-bs-title="{{ $account->displayTotalQsrBalance() }}">
+                                    {{ $account->displayTotalQsrBalance(2) }}
+                                </span>
+                            </td>
+                            <td>{{ $account->updated_at?->format(config('zenon.short_date_format')) }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div class="m-4 mt-2">
+                {{ $data->onEachSide(0)->links() }}
+            </div>
+        @elseif($data)
+            <div class="m-4 mt-4">
+                <x-alert
+                    message="No accounts found"
+                    type="info"
+                    icon="info-circle-fill"
+                    class="mb-0"
+                />
+            </div>
+        @endif
     </div>
 </div>
--- resources/views/livewire/explorer/tokens.blade.php
+++ resources/views/livewire/explorer/tokens.blade.php
@@ -60,59 +60,70 @@
         </div>
     </div>
     <div wire:loading.remove>
-        <div class="table-responsive">
-            <table class="table table-nowrap align-middle table-striped table-hover">
-                <thead>
-                <tr>
-                    <th>
-                        <button type="button" class="btn btn-sort" wire:click="sortBy('name')">
-                            <x-table-sort-button :sort="$sort" :order="$order" check="name"/>
-                        </button>
-                    </th>
-                    <th>
-                        <button type="button" class="btn btn-sort" wire:click="sortBy('holders_count')">
-                            <x-table-sort-button :sort="$sort" :order="$order" check="holders_count" title="Holders"/>
-                        </button>
-                    </th>
-                    <th>
-                        <button type="button" class="btn btn-sort" wire:click="sortBy('total_supply')">
-                            <x-table-sort-button :sort="$sort" :order="$order" check="total_supply"/>
-                        </button>
-                    </th>
-                    <th>
-                        <button type="button" class="btn btn-sort" wire:click="sortBy('token_standard')">
-                            <x-table-sort-button :sort="$sort" :order="$order" check="token_standard" title="Token Standard"/>
-                        </button>
-                    </th>
-                    <th>
-                        <button type="button" class="btn btn-sort" wire:click="sortBy('created_at')">
-                            <x-table-sort-button :sort="$sort" :order="$order" check="created_at" title="Created"/>
-                        </button>
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($data as $token)
+        @if ($data && $data->count())
+            <div class="table-responsive">
+                <table class="table table-nowrap align-middle table-striped table-hover">
+                    <thead>
                     <tr>
-                        <td>
-                            <a href=" {{ route('explorer.token', ['zts' => $token->token_standard]) }}">
-                                {{ $token->custom_label }}
-                            </a>
-                            <span class="fs-xs">
-                                {{ $token->symbol }}
-                            </span>
-                        </td>
-                        <td>{{ number_format($token->holders_count) }}</td>
-                        <td>{{ $token->getDisplayAmount($token->total_supply) }}</td>
-                        <td>{{ short_hash($token->token_standard, 8) }}</td>
-                        <td>{{ ($token->created_at ? $token->created_at?->format(config('zenon.short_date_format')) : '-') }}</td>
+                        <th>
+                            <button type="button" class="btn btn-sort" wire:click="sortBy('name')">
+                                <x-table-sort-button :sort="$sort" :order="$order" check="name"/>
+                            </button>
+                        </th>
+                        <th>
+                            <button type="button" class="btn btn-sort" wire:click="sortBy('holders_count')">
+                                <x-table-sort-button :sort="$sort" :order="$order" check="holders_count" title="Holders"/>
+                            </button>
+                        </th>
+                        <th>
+                            <button type="button" class="btn btn-sort" wire:click="sortBy('total_supply')">
+                                <x-table-sort-button :sort="$sort" :order="$order" check="total_supply"/>
+                            </button>
+                        </th>
+                        <th>
+                            <button type="button" class="btn btn-sort" wire:click="sortBy('token_standard')">
+                                <x-table-sort-button :sort="$sort" :order="$order" check="token_standard" title="Token Standard"/>
+                            </button>
+                        </th>
+                        <th>
+                            <button type="button" class="btn btn-sort" wire:click="sortBy('created_at')">
+                                <x-table-sort-button :sort="$sort" :order="$order" check="created_at" title="Created"/>
+                            </button>
+                        </th>
                     </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
-        <div class="m-4 mt-2">
-            {{ $data->onEachSide(0)->links() }}
-        </div>
+                    </thead>
+                    <tbody>
+                    @foreach($data as $token)
+                        <tr>
+                            <td>
+                                <a href=" {{ route('explorer.token', ['zts' => $token->token_standard]) }}">
+                                    {{ $token->custom_label }}
+                                </a>
+                                <span class="fs-xs">
+                                    {{ $token->symbol }}
+                                </span>
+                            </td>
+                            <td>{{ number_format($token->holders_count) }}</td>
+                            <td>{{ $token->getDisplayAmount($token->total_supply) }}</td>
+                            <td>{{ short_hash($token->token_standard, 8) }}</td>
+                            <td>{{ ($token->created_at ? $token->created_at?->format(config('zenon.short_date_format')) : '-') }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div class="m-4 mt-2">
+                {{ $data->onEachSide(0)->links() }}
+            </div>
+        @elseif($data)
+            <div class="m-4 mt-4">
+                <x-alert
+                    message="No tokens found"
+                    type="info"
+                    icon="info-circle-fill"
+                    class="mb-0"
+                />
+            </div>
+        @endif
     </div>
 </div>
