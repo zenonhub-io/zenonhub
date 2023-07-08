e6dad41393c4320f68909c6588bf5f8743917a21
DigitalSloth <digitals1oth@proton.me>
2023-07-08 13:54:33 +0100

    update explorer overview styling

--- resources/views/livewire/explorer/search.blade.php
+++ resources/views/livewire/explorer/search.blade.php
@@ -1,17 +1,19 @@
 <div>
-    <label for="explorer.filters.search" class="visually-hidden form-label">Search hash / height / address / token</label>
-    <div class="input-group input-group-merge input-group-lg">
-        <span class="input-group-prepend input-group-text">
-            <i class="bi-search"></i>
-        </span>
-        <input
-            type="text"
-            class="form-control"
-            id="explorer.filters.search"
-            placeholder="Search hash / height / address / token"
-            aria-label="Search hash / height / address / token"
-            wire:model.debounce.500ms="search"
-            value="{{ $search }}"
-        >
+    <div class="card shadow border-0 mb-4">
+        <label for="explorer.filters.search" class="visually-hidden form-label">Search hash / height / address / token</label>
+        <div class="input-group input-group-merge input-group-lg">
+            <span class="input-group-prepend input-group-text">
+                <i class="bi-search"></i>
+            </span>
+            <input
+                type="text"
+                class="form-control"
+                id="explorer.filters.search"
+                placeholder="Search hash / height / address / token"
+                aria-label="Search hash / height / address / token"
+                wire:model.debounce.500ms="search"
+                value="{{ $search }}"
+            >
+        </div>
     </div>
 </div>
--- resources/views/livewire/explorer/staking.blade.php
+++ resources/views/livewire/explorer/staking.blade.php
@@ -1,63 +1,65 @@
 <div>
-    <div class="card-header border-bottom">
-        <div class="d-block d-sm-flex align-items-center">
-            <div class="flex-grow-1">
-                <h4 class="mb-3 mb-sm-0">
-                    Staking
-                </h4>
-            </div>
-            <div class="d-block d-md-flex justify-content-md-end">
-                {{ ($data ? $data->links('vendor/livewire/top-links') : '') }}
+    <div class="card shadow">
+        <div class="card-header border-bottom">
+            <div class="d-block d-sm-flex align-items-center">
+                <div class="flex-grow-1">
+                    <h4 class="mb-3 mb-sm-0">
+                        Staking
+                    </h4>
+                </div>
+                <div class="d-block d-md-flex justify-content-md-end">
+                    {{ ($data ? $data->links('vendor/livewire/top-links') : '') }}
+                </div>
             </div>
         </div>
-    </div>
-    <div class="table-responsive">
-        <table class="table table-nowrap align-middle table-striped table-hover">
-            <thead>
-            <tr>
-                <th>
-                    Address
-                </th>
-                <th>
-                    <button type="button" class="btn btn-sort" wire:click="sortBy('amount')">
-                        <x-table-sort-button :sort="$sort" :order="$order" check="amount"/>
-                    </button>
-                </th>
-                <th>
-                    <button type="button" class="btn btn-sort" wire:click="sortBy('started_at')">
-                        <x-table-sort-button :sort="$sort" :order="$order" check="started_at" title="Started"/>
-                    </button>
-                </th>
-                <th>
-                    <button type="button" class="btn btn-sort" wire:click="sortBy('duration')">
-                        <x-table-sort-button :sort="$sort" :order="$order" check="duration" title="Lockup"/>
-                    </button>
-                </th>
-                <th>
-                    Available
-                </th>
-                <th>
-                    Duration
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($data as $stake)
+        <div class="table-responsive">
+            <table class="table table-nowrap align-middle table-striped table-hover">
+                <thead>
                 <tr>
-                    <td>
-                        <x-address :account="$stake->account" :eitherSide="8" :alwaysShort="true"/>
-                    </td>
-                    <td>{{ $stake->display_amount }} ZNN</td>
-                    <td>{{ $stake->started_at->format(config('zenon.short_date_format')) }}</td>
-                    <td>{{ $stake->display_duration }}</td>
-                    <td>{{ $stake->end_date->format(config('zenon.short_date_format')) }}</td>
-                    <td>{{ $stake->started_at->diffForHumans(['parts' => 2], true) }}</td>
+                    <th>
+                        Address
+                    </th>
+                    <th>
+                        <button type="button" class="btn btn-sort" wire:click="sortBy('amount')">
+                            <x-table-sort-button :sort="$sort" :order="$order" check="amount"/>
+                        </button>
+                    </th>
+                    <th>
+                        <button type="button" class="btn btn-sort" wire:click="sortBy('started_at')">
+                            <x-table-sort-button :sort="$sort" :order="$order" check="started_at" title="Started"/>
+                        </button>
+                    </th>
+                    <th>
+                        <button type="button" class="btn btn-sort" wire:click="sortBy('duration')">
+                            <x-table-sort-button :sort="$sort" :order="$order" check="duration" title="Lockup"/>
+                        </button>
+                    </th>
+                    <th>
+                        Available
+                    </th>
+                    <th>
+                        Duration
+                    </th>
                 </tr>
-            @endforeach
-            </tbody>
-        </table>
-    </div>
-    <div class="m-4 mt-2">
-        {{ $data->onEachSide(0)->links() }}
+                </thead>
+                <tbody>
+                @foreach($data as $stake)
+                    <tr>
+                        <td>
+                            <x-address :account="$stake->account" :eitherSide="8" :alwaysShort="true"/>
+                        </td>
+                        <td>{{ $stake->display_amount }} ZNN</td>
+                        <td>{{ $stake->started_at->format(config('zenon.short_date_format')) }}</td>
+                        <td>{{ $stake->display_duration }}</td>
+                        <td>{{ $stake->end_date->format(config('zenon.short_date_format')) }}</td>
+                        <td>{{ $stake->started_at->diffForHumans(['parts' => 2], true) }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="m-4 mt-2">
+            {{ $data->onEachSide(0)->links() }}
+        </div>
     </div>
 </div>
