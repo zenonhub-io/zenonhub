dbe735ec3e8f943f144b78765d10e9c91889b8e5
DigitalSloth <digitals1oth@proton.me>
2023-07-08 17:52:00 +0100

    refactor az stats into individual components

---
+++ app/Http/Livewire/Stats/Accelerator/Funding.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Livewire\Stats\Accelerator;
+
+use App\Http\Livewire\ChartTrait;
+use App\Models\Nom\Account;
+use Livewire\Component;
+
+class Funding extends Component
+{
+    use ChartTrait;
+
+    public Account $acceleratorContract;
+
+    public function render()
+    {
+        return view('livewire.stats.accelerator.funding');
+    }
+
+    public function loadFundingData()
+    {
+        $this->acceleratorContract = Account::findByAddress(Account::ADDRESS_ACCELERATOR);
+        $znnToken = znn_token();
+        $qsrToken = qsr_token();
+
+        $totalZnnUsed = $this->acceleratorContract
+            ->sent_blocks()
+            ->where('token_id', $znnToken->id)
+            ->sum('amount');
+
+        $totalQsrUsed = $this->acceleratorContract
+            ->sent_blocks()
+            ->where('token_id', $qsrToken->id)
+            ->sum('amount');
+
+        $fundingLabels = [
+            'Remaining',
+            'Used',
+        ];
+
+        $znnFunds = [
+            'labels' => $fundingLabels,
+            'data' => [
+                (float) filter_var($this->acceleratorContract->display_znn_balance, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION),
+                (float) filter_var($znnToken->getDisplayAmount($totalZnnUsed), FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION),
+            ],
+        ];
+
+        $qsrFunds = [
+            'labels' => $fundingLabels,
+            'data' => [
+                (float) filter_var($this->acceleratorContract->display_qsr_balance, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION),
+                (float) filter_var($qsrToken->getDisplayAmount($totalQsrUsed), FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION),
+            ],
+        ];
+
+        $this->emit('stats.az.fundingDataLoaded', [
+            'znn' => $znnFunds,
+            'qsr' => $qsrFunds,
+        ]);
+    }
+}
