0dbfb65a151f51d55bfeef7228dd1a18b399a3fe
DigitalSloth <digitals1oth@proton.me>
2023-07-14 08:02:43 +0100

    cache az stats

--- app/Http/Livewire/Stats/Accelerator/Projects.php
+++ app/Http/Livewire/Stats/Accelerator/Projects.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\Stats\Accelerator;
 
 use App\Http\Livewire\ChartTrait;
 use App\Models\Nom\AcceleratorProject;
+use Illuminate\Support\Facades\Cache;
 use Livewire\Component;
 
 class Projects extends Component
@@ -19,15 +20,18 @@ class Projects extends Component
 
     public function loadProjectData()
     {
-        $this->projectData = [
-            'labels' => ['New', 'Accepted', 'Completed', 'Rejected'],
-            'data' => [
-                AcceleratorProject::isNew()->count(),
-                AcceleratorProject::isAccepted()->count(),
-                AcceleratorProject::isCompleted()->count(),
-                AcceleratorProject::isRejected()->count(),
-            ],
-        ];
+        $cacheExpiry = (60 * 60);
+        $this->projectData = Cache::remember('stats.az.projectTotals', $cacheExpiry, function () {
+            return [
+                'labels' => ['New', 'Accepted', 'Completed', 'Rejected'],
+                'data' => [
+                    AcceleratorProject::isNew()->count(),
+                    AcceleratorProject::isAccepted()->count(),
+                    AcceleratorProject::isCompleted()->count(),
+                    AcceleratorProject::isRejected()->count(),
+                ],
+            ];
+        });
 
         $this->emit('stats.az.projectDataLoaded', $this->projectData);
     }
