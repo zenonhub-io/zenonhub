fd3a9b922b3928632dfb4b00bade4ec4c64ceb8a
DigitalSloth <digitals1oth@proton.me>
2023-07-29 14:02:50 +0100

    reprocess blocks with missing decoded data

---
+++ app/Actions/ProcessUnprocessedBlocks.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\Nom\AccountBlock;
+use App\Models\Nom\ContractMethod;
+use DigitalSloth\ZnnPhp\Utilities as ZnnUtilities;
+use Spatie\QueueableAction\QueueableAction;
+
+class ProcessUnprocessedBlocks
+{
+    use QueueableAction;
+
+    public function __construct()
+    {
+    }
+
+    public function execute(): void
+    {
+        AccountBlock::whereHas('data', fn ($q) => $q->whereNotNull('raw')
+            ->whereNull('decoded')
+            ->where('is_processed', '0')
+        )->chunk(200, function ($blocks) {
+            foreach ($blocks as $block) {
+                $data = base64_decode($block->data->raw);
+
+                if (in_array($block->block_type, [AccountBlock::TYPE_SEND, AccountBlock::TYPE_CONTRACT_SEND])) {
+
+                    $contract = $block->to_account->contract;
+                    $fingerprint = ZnnUtilities::getDataFingerprint($data);
+                    $contractMethod = ContractMethod::where('contract_id', $contract?->id)
+                        ->where('fingerprint', $fingerprint)
+                        ->first();
+
+                    if (! $contractMethod) {
+                        $contractMethod = ContractMethod::whereHas('contract', fn ($q) => $q->where('name', 'Common'))
+                            ->where('fingerprint', $fingerprint)
+                            ->first();
+                    }
+
+                    if ($contractMethod) {
+                        $block->contract_method_id = $contractMethod->id;
+                        $block->save();
+
+                        $contractName = ucfirst(strtolower($contractMethod->contract->name));
+                        $embeddedContract = "DigitalSloth\ZnnPhp\Abi\Contracts\\".$contractName;
+
+                        if (class_exists($embeddedContract)) {
+                            $embeddedContract = new $embeddedContract();
+                            $decoded = $embeddedContract->decode($contractMethod->name, $data);
+                            $parameters = $embeddedContract->getParameterNames($contractMethod->name);
+
+                            if ($decoded && $parameters) {
+                                $parameters = explode(',', $parameters);
+
+                                $block->data->decoded = array_combine(
+                                    $parameters,
+                                    $decoded
+                                );
+                                $block->data->save();
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
