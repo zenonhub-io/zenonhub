b75b838fc62971abc554e8d557287f7c29c53b22
DigitalSloth <digitals1oth@proton.me>
2023-07-29 11:13:10 +0100

    Change home page stats

--- app/Http/Controllers/Site/Home.php
+++ app/Http/Controllers/Site/Home.php
@@ -13,12 +13,12 @@ class Home extends PageController
     public function show()
     {
         $this->page['data']['stats'] = [
-            'pillars' => Pillar::isActive()->count(),
-            'sentinels' => Sentinel::isActive()->count(),
             'momentums' => short_number(Cache::get('momentum-count')),
             'transactions' => short_number(Cache::get('transaction-count')),
             'addresses' => short_number(Cache::get('address-count')),
             'delegators' => number_format((float) Cache::get('delegators-count')),
+            'pillars' => Pillar::isActive()->count(),
+            'sentinels' => Sentinel::isActive()->count(),
         ];
         $this->page['data']['accelerator'] = AcceleratorProject::orderByLatest()->limit(8)->get();
 
