db1b1fb766796c61f9afed902b524edad83c50ef
DigitalSloth <digitals1oth@proton.me>
2023-08-08 05:45:38 +0100

    refactor whale alerts

---
+++ app/Bots/WhaleAlerts.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Bots;
+
+use Illuminate\Notifications\Notifiable;
+
+class WhaleAlerts
+{
+    use Notifiable;
+
+    public function routeNotificationForTwitter($notification)
+    {
+        return [
+            config('whale-alerts.twitter.consumer_key'),
+            config('whale-alerts.twitter.consumer_secret'),
+            config('whale-alerts.twitter.access_token'),
+            config('whale-alerts.twitter.access_token_secret'),
+        ];
+    }
+
+    public function routeNotificationForTelegram()
+    {
+        return config('whale-alerts.telegram.chat');
+    }
+
+    public function routeNotificationForDiscordWebhook()
+    {
+        return config('whale-alerts.discord.webhook');
+    }
+}
