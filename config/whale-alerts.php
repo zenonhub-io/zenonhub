6048dc012f1cecfa094c4fc77366d7cd43f12545
DigitalSloth <digitals1oth@proton.me>
2023-08-08 05:45:38 +0100

    refactor whale alerts

--- config/whale-alerts.php
+++ config/whale-alerts.php
@@ -1,26 +1,21 @@
 <?php
 
 return [
-    'znn_cutoff' => env('WHALE_BOT_ZNN_LIMIT', 1000),
-    'qsr_cutoff' => env('WHALE_BOT_qsr_LIMIT', 10000),
+    'znn_cutoff' => env('WHALE_BOT_ZNN_LIMIT', 1000) * 100000000,
+    'qsr_cutoff' => env('WHALE_BOT_qsr_LIMIT', 10000) * 100000000,
     'discord' => [
         'enabled' => env('WHALE_BOT_ENABLE_DISCORD'),
-        'bot-token' => env('WHALE_BOT_DISCORD_BOT_TOKEN'),
-        'channels' => [
-            env('WHALE_BOT_DISCORD_CHANNEL_ID'),
-        ],
+        'webhook' => env('WHALE_BOT_DISCORD_WEBHOOK'),
     ],
     'telegram' => [
         'enabled' => env('WHALE_BOT_ENABLE_TELEGRAM'),
         'bot-token' => env('WHALE_BOT_TELEGRAM_BOT_TOKEN'),
-        'channels' => [
-            env('WHALE_BOT_DISCORD_CHANNEL_ID'),
-        ],
+        'chat' => env('WHALE_BOT_TELEGRAM_CHAT_ID'),
     ],
     'twitter' => [
         'enabled' => env('WHALE_BOT_ENABLE_TWITTER'),
-        'api_key' => env('WHALE_BOT_TWITTER_API_KEY'),
-        'api_key_secret' => env('WHALE_BOT_TWITTER_API_SECRET'),
+        'consumer_key' => env('WHALE_BOT_TWITTER_CONSUMER_KEY'),
+        'consumer_secret' => env('WHALE_BOT_TWITTER_CONSUMER_SECRET'),
         'access_token' => env('WHALE_BOT_TWITTER_ACCESS_TOKEN'),
         'access_token_secret' => env('WHALE_BOT_TWITTER_ACCESS_TOKEN_SECRET'),
     ],
