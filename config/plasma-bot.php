bf82836bfe7aa615327af90ff052c3e4cfa73a4b
DigitalSloth <digitals1oth@proton.me>
2023-08-08 05:45:49 +0100

    remove old config value

--- config/plasma-bot.php
+++ config/plasma-bot.php
@@ -4,6 +4,5 @@ return [
     'node_url' => env('PLASMA_BOT_NODE', 'wss://node.zenonhub.io:35998'),
     'keystore' => env('PLASMA_BOT_KEYSTORE'),
     'passphrase' => env('PLASMA_BOT_PASSPHRASE'),
-    'mnemonic' => env('PLASMA_BOT_MNEMONIC'),
     'address' => env('PLASMA_BOT_ADDRESS'),
 ];
