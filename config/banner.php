a030fc3477db8804e5c7a3c566801f40f7a7bc8f
DigitalSloth <digitals1oth@proton.me>
2023-08-12 20:03:38 +0100

    renable banner

--- config/banner.php
+++ config/banner.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'enabled' => false,
+    'enabled' => true,
     'message' => 'The explorer has been paused and some services will be unavailable while our nodes re-sync, view their status <a class="alert-link" href="/nodes?tab=sync">here</a>',
     'class' => 'info',
     'icon' => 'info-circle',
