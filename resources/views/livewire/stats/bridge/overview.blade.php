508fe332895eedc5d13f147ac24d903eab506fd0
DigitalSloth <digitals1oth@proton.me>
2023-09-03 19:53:42 +0100

    better bridge status

--- resources/views/livewire/stats/bridge/overview.blade.php
+++ resources/views/livewire/stats/bridge/overview.blade.php
@@ -1,22 +1,38 @@
 <div wire:init="loadLiquidityData">
-    <div class="bg-secondary shadow rounded-2 p-3 mb-4">
+    @if ($halted)
+        <x-alert
+            message="The bridge is currently halted, please wait until it is back online before interacting with it"
+            type="warning"
+            icon="exclamation-octagon"
+            class="d-flex justify-content-center mb-4"
+        />
+    @elseif ($orchestrators < 66)
+        <x-alert
+            message="Only {{$orchestrators}}% of orchestrators are online, please wait until there are over 66% before interactive with the bridge"
+            type="warning"
+            icon="exclamation-octagon"
+            class="d-flex justify-content-center mb-4"
+        />
+    @endif
+
+    <div class="bg-secondary shadow rounded-2 p-3">
         <div class="d-block d-md-flex justify-content-md-evenly">
             <div class="text-start text-md-center mb-2 mb-md-0">
-                <span class="d-inline d-md-block text-muted fs-sm">Status</span>
+                <span class="d-inline d-md-block text-muted fs-sm">Bridge <span class="text-muted"><i class="bi-question-circle" data-bs-toggle="tooltip" data-bs-title="Indicates if the bridge is online or has been halted"></i></span></span>
                 <span class="float-end float-md-none">
                     <span class="legend-indicator bg-{{ (! $halted ? 'success' : 'danger') }}" data-bs-toggle="tooltip" data-bs-title="{{ (! $halted ? 'Active' : 'Halted') }}"></span>
                 </span>
             </div>
             <div class="text-start text-md-center mb-2 mb-md-0">
-                <span class="d-inline d-md-block text-muted fs-sm">Orchestrators</span>
+                <span class="d-inline d-md-block text-muted fs-sm">Orchestrators <span class="text-muted"><i class="bi-question-circle" data-bs-toggle="tooltip" data-bs-title="Over 66% of orchestrators need to be online for the bridge to function"></i></span></span>
                 <span class="float-end float-md-none">
-                    <span class="legend-indicator bg-{{ ($orchestratorsOnline > 66 ? 'success' : 'danger') }}" data-bs-toggle="tooltip" data-bs-title="{{ $orchestratorsOnline }}% Online"></span>
+                    <span class="legend-indicator bg-{{ ($orchestrators > 66 ? 'success' : 'danger') }}" data-bs-toggle="tooltip" data-bs-title="{{ $orchestrators }}% Online"></span>
                 </span>
             </div>
             <div class="text-start text-md-center mb-2 mb-md-0">
-                <span class="d-inline d-md-block text-muted fs-sm">Admin</span>
+                <span class="d-inline d-md-block text-muted fs-sm">Admin <span class="text-muted"><i class="bi-question-circle" data-bs-toggle="tooltip" data-bs-title="The admin address allowed to issue commands to the bridge"></i></span></span>
                 <span class="float-end float-md-none">
-                    <x-address :account="$adminAddress" :eitherSide="10" breakpoint="lg"/>
+                    <x-address :account="$adminAddress" :eitherSide="10" breakpoint="lg" :named="false"/>
                 </span>
             </div>
         </div>
@@ -37,10 +53,12 @@
     </div>
     <div class="row">
         <div class="col-24 text-center">
-            <a href="{{ $affiliateLink }}" target="_blank" class="btn btn-lg btn-outline-primary">
-                Bridge tokens now
-                <i class="bi bi-arrow-right ms-2"></i>
-            </a>
+            @if (! $halted && $orchestrators > 66)
+                <a href="{{ $affiliateLink }}" target="_blank" class="btn btn-lg btn-outline-primary mt-4">
+                    Bridge tokens now
+                    <i class="bi bi-arrow-right ms-2"></i>
+                </a>
+            @endif
         </div>
     </div>
 </div>
