1af31cf3dadbfd5ee661fe1119bb4d7398639b70
DigitalSloth <digitals1oth@proton.me>
2023-10-16 17:53:05 +0100

    refactor zenon service

--- app/Services/ZenonSdk.php
+++ app/Services/ZenonSdk.php
@@ -2,20 +2,46 @@
 
 namespace App\Services;
 
+use App\Models\Nom\Account;
+use DigitalSloth\ZnnPhp\Utilities;
 use DigitalSloth\ZnnPhp\Zenon;
 
 class ZenonSdk
 {
-    protected Zenon $zenon;
+    protected Zenon $zenonSdk;
 
     public function __construct(
         private readonly ?string $node
     ) {
-        $this->zenon = new Zenon($this->node, config('zenon.throw_api_errors'));
+        $this->zenonSdk = new Zenon($this->node, config('zenon.throw_api_errors'));
     }
 
-    public function getZenon(): Zenon
+    public function getSdk(): Zenon
     {
-        return $this->zenon;
+        return $this->zenonSdk;
+    }
+
+    public static function verifySignature(string $publicKey, string $address, string $message, string $signature): bool
+    {
+        $validated = false;
+        $validSignature = Utilities::verifySignedMessage(
+            $publicKey,
+            $message,
+            $signature
+        );
+
+        $account = Account::findByAddress($address);
+        $accountCheck = Utilities::addressFromPublicKey($publicKey);
+
+        if ($validSignature && ($address === $accountCheck)) {
+            $validated = true;
+        }
+
+        if ($validated && $account && ! $account->public_key) {
+            $account->public_key = base64_encode(Utilities::hexToBin($publicKey));
+            $account->save();
+        }
+
+        return $validated;
     }
 }
