f1629e1888fec4fe1d1dc57ddfeae5ed88a1adc4
DigitalSloth <digitals1oth@proton.me>
2023-10-18 17:08:12 +0100

    update notification wording

--- resources/views/mail/notifications/nom/sentinel/registered.blade.php
+++ resources/views/mail/notifications/nom/sentinel/registered.blade.php
@@ -1,5 +1,5 @@
 @component('mail::message')
-# {{ $sentinel->owner->address }} has been registered
+# A new sentinel has been registered!
 
 @component('mail::button', ['url' => route('explorer.account', [
     'address' => $sentinel->owner->address,
