0b3fcf9b3b9b77a38329f3c9c93f1ef6cf2bc4b6
DigitalSloth <digitals1oth@proton.me>
2024-09-29 21:08:25 +0100

    wip: site search

---
+++ app/Livewire/SiteSearch.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SiteSearch extends Component
+{
+    public function render()
+    {
+        return view('livewire.site-search');
+    }
+}
---
+++ resources/views/livewire/site-search.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <div class="modal-body">
+        <div class="input-group input-group-inline rounded-pill">
+            <span class="input-group-text rounded-start-pill">
+                <i class="bi bi-search me-2"></i>
+            </span>
+            <input type="search" class="form-control ps-0 rounded-end-pill" placeholder="Search" aria-label="Search">
+        </div>
+    </div>
+</div>
