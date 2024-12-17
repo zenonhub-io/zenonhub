d95461e21f5e5d8422b3dd69c55192f32c25cfc5
DigitalSloth <digitals1oth@proton.me>
2024-12-17 17:17:07 +0000

    wip: table views

--- resources/views/vendor/livewire-tables/components/tools.blade.php
+++
@@ -1,8 +0,0 @@
-@aware(['component','isTailwind','isBootstrap'])
-
-<div @class([
-    'flex-col' => $isTailwind,
-    'd-flex flex-column ' => ($isBootstrap),
-])>
-    {{ $slot }}
-</div>
--- resources/views/vendor/livewire-tables/includes/debug.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-    @if ($this->debugIsEnabled())
-        <p><strong>@lang('Debugging Values'):</strong></p>
-
-        @if (! app()->runningInConsole())
-            <div class="mb-4">@dump((new \Rappasoft\LaravelLivewireTables\DataTransferObjects\DebuggableData($this))->toArray())</div>
-        @endif
-    @endif
-</div>
