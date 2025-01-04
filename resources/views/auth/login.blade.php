1c913d0d79a1d8ca71216379820647a349ede350
DigitalSloth <digitals1oth@proton.me>
2025-01-04 22:53:05 +0000

    fix: auth layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,8 @@
 <x-auth-layout>
-    <x-includes.header :title="__('Sign in!')" centered>
-        <ul class="nav nav-tabs nav-fill nav-tabs-flush gap-6 overflow-x border-0">
+
+    <header class="header mb-6 border-bottom-0 border-bottom-md">
+        <x-includes.header-title :title="__('Sign in!')" class="mb-4" />
+        <ul class="nav nav-tabs nav-fill nav-tabs-flush gap-6 overflow-x border-0 lead">
             <li class="nav-item">
                 <x-link :href="route('login')" class="nav-link active">{{ __('Login') }}</x-link>
             </li>
@@ -8,7 +10,7 @@
                 <x-link :href="route('register')" class="nav-link">{{ __('Register') }}</x-link>
             </li>
         </ul>
-    </x-includes.header>
+    </header>
 
     @if (session('status'))
         <x-alerts.alert type="success" class="mb-4">
