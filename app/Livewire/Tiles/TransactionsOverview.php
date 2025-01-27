14263bc7b1e3fc3de401da123089507d99dd474d
digitalSloth <digitals1oth@proton.me>
2025-01-27 20:14:43 +0000

    feature: better caching

--- app/Livewire/Tiles/TransactionsOverview.php
+++ app/Livewire/Tiles/TransactionsOverview.php
@@ -14,7 +14,10 @@ class TransactionsOverview extends BaseComponent
 {
     public function render(): View
     {
-        return view('livewire.tiles.transactions-overview', $this->getStats());
+        $stats = $this->getStats();
+        $stats['latest'] = AccountBlock::latest('id')->first();
+
+        return view('livewire.tiles.transactions-overview', $stats);
     }
 
     private function getStats(): array
@@ -22,7 +25,6 @@ class TransactionsOverview extends BaseComponent
         return Cache::remember('tile.transactions-overview', now()->addMinutes(10), fn () => [
             'total' => Number::abbreviate(AccountBlock::max('id'), 2),
             'daily' => Number::abbreviate(AccountBlock::whereDate('created_at', now())->count(), 2),
-            'latest' => AccountBlock::latest('id')->first(),
         ]);
     }
 }
