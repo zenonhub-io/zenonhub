f0fb4c6198284d5dd401ac3f89274c0d6e40da68
digitalSloth <digitals1oth@proton.me>
2025-03-05 10:52:47 +0000

    fix: hqz ui changes

--- resources/views/components/includes/navbar/stats.blade.php
+++ resources/views/components/includes/navbar/stats.blade.php
@@ -6,11 +6,14 @@
     <span class="text-heading fw-semibold">Bridge:</span>
     <span class="text-muted"><livewire:utilities.bridge-status /></span>
 </div>
-<div class="gap-1 text-xs d-none d-lg-flex">
-    <span class="text-heading fw-semibold">ZNN:</span>
-    <span class="text-primary">${{ number_format(app('znnToken')->price, 2) }}</span>
-</div>
-<div class="gap-1 text-xs d-none d-lg-flex">
-    <span class="text-heading fw-semibold">QSR:</span>
-    <span class="text-secondary">${{ number_format(app('qsrToken')->price, 2) }}</span>
-</div>
+
+@if (app('currentChain')->code === 'nom')
+    <div class="gap-1 text-xs d-none d-lg-flex">
+        <span class="text-heading fw-semibold">{{ app('znnToken')->symbol }}:</span>
+        <span class="text-primary">${{ number_format(app('znnToken')->price, 2) }}</span>
+    </div>
+    <div class="gap-1 text-xs d-none d-lg-flex">
+        <span class="text-heading fw-semibold">{{ app('qsrToken')->symbol }}:</span>
+        <span class="text-secondary">${{ number_format(app('qsrToken')->price, 2) }}</span>
+    </div>
+@endif
