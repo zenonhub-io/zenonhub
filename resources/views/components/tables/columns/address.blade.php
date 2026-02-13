8c1fdd6e0d2bb55644bec90e5576fe8754a47036
digitalSloth <digitals1oth@proton.me>
2026-02-13 17:56:43 +0000

    feat: shorten address

--- resources/views/components/tables/columns/address.blade.php
+++ resources/views/components/tables/columns/address.blade.php
@@ -7,7 +7,7 @@
 ])
 
 @if ($row instanceof \App\Models\Nom\Account)
-    <x-address :account="$row" :always-short="$alwaysShort" :linked="$linked" :breakpoint="$breakpoint" :named="$named" :copyable="$copyable" />
+    <x-address :account="$row" :always-short="$alwaysShort" :linked="$linked" :breakpoint="$breakpoint" :named="$named" :copyable="$copyable" :either-side="6" />
 @else
-    <x-address :account="load_account($row->address)" :always-short="$alwaysShort" :linked="$linked" :breakpoint="$breakpoint" :named="$named" :copyable="$copyable" />
+    <x-address :account="load_account($row->address)" :always-short="$alwaysShort" :linked="$linked" :breakpoint="$breakpoint" :named="$named" :copyable="$copyable" :either-side="6" />
 @endif
